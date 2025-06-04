9a82514d6a0f2a742c37e46479b31f6385e13070
Yarik_Killer-Pico <sologubaroslav504@gmail.com>
2025-06-04 20:41:58 +0300

    Header

---
+++ vue-project/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+<header>
+<h1>Мій перший Vue-додаток!</h1>
+</header>
+<script>
+export default {
+    name: "HeaderComponent"
+};
+</script>
+<style scoped>
+header {
+    background: #42b983;
+    color: white;
+    padding: 15px;
+    text-align: center;
+}
+</style>
