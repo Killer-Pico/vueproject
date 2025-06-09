63a8ac4300617ecca18b732f16e04450fc2b4e52
Yarik_Killer-Pico <sologubaroslav504@gmail.com>
2025-06-09 13:07:53 +0300

    Add style

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -10,7 +10,7 @@
 <script>
 import BusinessCard from './components/BusinessCard.vue';
 import HeaderComponent from './components/Header.vue';
-import MessageComponent from './components/MessageComponent.vue'
+import MessageComponent from './components/MessageComponent.vue';
 
 export default {
   name: 'App',
--- vue-project/src/components/BusinessCard.vue
+++ vue-project/src/components/BusinessCard.vue
@@ -21,6 +21,21 @@ export default {
 };
 </script>
 <style>
-
+.message-box{
+    border: 2px solid #42b983;
+    border-radius: 5px;
+    padding: 15px;
+    margin: 10px auto;
+    width: 350px;
+    text-align: center;
+    font-size:18px;
+    color:#333;
+    background-color: #f0f9f5;
+}
+h2 {
+    color: #2c3e50;
+    font-size: 22px;
+    margin-bottom: 10px;
+}
 </style>
 
